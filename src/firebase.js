6812a940246cd5ac9e97ac2065217cf619fb3201
kul580145 <kul580145@gmail.com>
2026-03-15 21:56:39 +0100

    Update Firebase configuration for JobPilot Italia 1

--- src/firebase.js
+++ src/firebase.js
@@ -6,9 +6,9 @@ import { getFirestore } from "firebase/firestore";
 // JobPilot Italia 1 এর configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB8YP8yurdpHdLVZzi1MJQBzXf3Q2vQ9aA",
-  authDomain: "jobpilot-italia-2.firebaseapp.com",
-  projectId: "jobpilot-italia-2",
-  storageBucket: "jobpilot-italia-2.appspot.com",
+  authDomain: "jobpilot-italia-1.firebaseapp.com",
+  projectId: "jobpilot-italia-1",
+  storageBucket: "jobpilot-italia-1.appspot.com",
   messagingSenderId: "273453116522",
   appId: "1:273453116522:web:5183b8ef1152b4a4c8a4b9",
 };
