1e9341a60f6b8e99f38d6cefa4f09be53a9afef5
kul580145 <kul580145@gmail.com>
2026-03-15 21:55:42 +0100

    Add Firebase configuration and initialization

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+// JobPilot Italia 1 এর configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyB8YP8yurdpHdLVZzi1MJQBzXf3Q2vQ9aA",
+  authDomain: "jobpilot-italia-2.firebaseapp.com",
+  projectId: "jobpilot-italia-2",
+  storageBucket: "jobpilot-italia-2.appspot.com",
+  messagingSenderId: "273453116522",
+  appId: "1:273453116522:web:5183b8ef1152b4a4c8a4b9",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const googleProvider = new GoogleAuthProvider();
